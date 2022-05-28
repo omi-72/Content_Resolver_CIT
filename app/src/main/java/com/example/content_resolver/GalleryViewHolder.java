09a82e0ddf7e515d2b505a19aa9f4a5e9e13b449
omi-72 <jannat.omi722@gmail.com>
2022-05-28 22:48:16 +0600

    add item findViewByid

--- app/src/main/java/com/example/content_resolver/GalleryViewHolder.java
+++ app/src/main/java/com/example/content_resolver/GalleryViewHolder.java
@@ -1,12 +1,22 @@
 package com.example.content_resolver;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class GalleryViewHolder extends RecyclerView.ViewHolder {
+
+    ImageView g_image;
+
+    TextView g_image_name;
+
     public GalleryViewHolder(@NonNull View itemView) {
         super(itemView);
+
+        g_image = itemView.findViewById(R.id.g_image);
+        g_image_name= itemView.findViewById(R.id.g_image_name);
     }
 }
