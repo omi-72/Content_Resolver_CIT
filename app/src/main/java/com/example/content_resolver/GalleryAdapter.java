2ce5de30e2cb572bacadf9c5e4a438bd92ea610b
omi-72 <jannat.omi722@gmail.com>
2022-05-28 22:58:32 +0600

    Set Context or List<Gallery> galleryList & set constructor method

--- app/src/main/java/com/example/content_resolver/GalleryAdapter.java
+++ app/src/main/java/com/example/content_resolver/GalleryAdapter.java
@@ -1,11 +1,23 @@
 package com.example.content_resolver;
 
+import android.content.Context;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class GalleryAdapter extends RecyclerView.Adapter<GalleryViewHolder> {
+
+    Context context;
+    List<Gallery> galleryList;
+
+    public GalleryAdapter(Context context, List<Gallery> galleryList) {
+        this.context = context;
+        this.galleryList = galleryList;
+    }
+
     @NonNull
     @Override
     public GalleryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
