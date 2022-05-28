02b1286dcf23562b3a79baeeaa2e70bb47146788
omi-72 <jannat.omi722@gmail.com>
2022-05-28 22:54:07 +0600

    add galleryAdapter on extend RecyclerView<GalleryViewHolder> & implement method

--- app/src/main/java/com/example/content_resolver/GalleryAdapter.java
+++ app/src/main/java/com/example/content_resolver/GalleryAdapter.java
@@ -1,4 +1,24 @@
 package com.example.content_resolver;
 
-public class GalleryAdapter {
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GalleryAdapter extends RecyclerView.Adapter<GalleryViewHolder> {
+    @NonNull
+    @Override
+    public GalleryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GalleryViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
